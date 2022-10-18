05eaf10e78939b2127c87519308ca7dfb6adfa40
Amanda Portela <mandaptela@gmail.com>
2022-10-18 17:45:33 -0300

    add a space only for the image

--- src/Components/SobreMim.js
+++ src/Components/SobreMim.js
@@ -7,14 +7,18 @@ class SobreMim extends React.Component {
     return (
     <>
       <section id="sobre">
+        <div id="space-photo">
           <img id="foto" alt="foto-perfil" src={foto}/>
-            <p id="sobre-title"> Sobre </p>
-            <p id="sobre-text">
-            Estudante de Desenvolvimento Web na
-            <strong> Trybe</strong>. <br />
-            Adoro <strong>música, gatos e documentários aleatórios</strong>; <br/>
-            e jogos como <strong>Don't Starve Together</strong>, <strong>Minecraft</strong>, <strong>Wild Rift</strong> e <strong>Card/Board Games</strong>!
-            </p>
+        </div>
+        <div className="texto-sobre">
+          <p id="sobre-title"> Sobre </p>
+          <p id="sobre-text">
+          Estudante de Desenvolvimento Web na
+          <strong> Trybe</strong>. <br />
+          Adoro <strong>música, gatos e documentários aleatórios</strong>; <br/>
+          e jogos como <strong>Don't Starve Together</strong>, <strong>Minecraft</strong>, <strong>Wild Rift</strong> e <strong>Card/Board Games</strong>!
+          </p>
+        </div>
       </section>
     </>
     )
