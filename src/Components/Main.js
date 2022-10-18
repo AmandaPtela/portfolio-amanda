403fdc98774a6bfd1700bdfadd90de7c77711590
Amanda Portela <mandaptela@gmail.com>
2022-10-18 17:44:59 -0300

    navigation out

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from "react-router-dom";
 import Header from './Header';
 import Content from './Content';
 import './CSS/Main.css';
@@ -8,10 +7,6 @@ class Main extends React.Component {
   render() {
     return (
       <div id="all">
-        <nav id="menu">
-          <Link to="/Projetos" className="links-nav">Projetos</Link>
-          <Link className="links-nav">Contato</Link>
-        </nav>
         <div className="app">
           <Header />
           <Content />
