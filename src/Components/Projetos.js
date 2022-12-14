666b9485f5f3eca4fd5c55b1ec3bf9075e2c3e81
Amanda Portela <mandaptela@gmail.com>
2022-12-14 18:26:07 -0300

    ajuste link modal

--- src/Components/Projetos.js
+++ src/Components/Projetos.js
@@ -10,7 +10,7 @@ function Projetos() {
     { nome:'Deck Maker',
       descricao: 'Este projeto é um construtor de baralho, onde o usuário consegue criar um baralho, adicionar e remover uma carta do baralho criado, visualizar todas as cartas que foram adicionadas ao baralho e filtrar as cartas do baralho',
       linkDeploy: "https://deck-maker-app.vercel.app/", linkRepo: 'https://github.com/AmandaPtela/deck-maker', image: {img1} },
-    { nome:'Portfólio', descricao: '- Em desenvolvimento - ', linkDeploy: 'https://portfolio-seven-xi-63.vercel.app/', linkRepo: 'https://github.com/AmandaPtela/portfolio-amanda', image: {img} },
+    { nome:'Portfólio', descricao: '- Em desenvolvimento - ', linkDeploy: 'https://portfolio-amandaptela.vercel.app/', linkRepo: 'https://github.com/AmandaPtela/portfolio-amanda', image: {img} },
     { nome:'Lectures - Node', descricao: '- Em desenvolvimento - Repositório feito para consultas durante estudos sobre NodeJS.', linkDeploy: '', linkRepo: 'https://github.com/AmandaPtela/lectures-node', image: {img} },
     {nome:'SoundTune', descricao: '- Em desenvolvimento - Aplicação para buscar músicas, álbuns e artistas, também podendo adicioná-los como favoritos.', linkDeploy: '', linkRepo: '',  image: {img} }]
 
