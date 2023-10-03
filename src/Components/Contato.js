8cac245ba48f6ab50d57f98aff4b741c529b4161
Amanda Portela <mandaptela@gmail.com>
2023-10-03 15:21:17 -0300

    Ajute página de contatos

--- src/Components/Contato.js
+++ src/Components/Contato.js
@@ -10,21 +10,15 @@ class Contato extends React.Component {
         <section id="contatos">
           <div><p id="contato-title"> Contato </p></div>
           <div id="links-contatos">
-            <a href="https://instagram.com/manda1_6"><img  alt="" height="10px" width="10px" id="rede-1"
-              src="https://img.shields.io/badge/-Instagram-%23E4405F?style=for-the-badge&logo=instagram&logoColor=white"
-              target="_blank"/>
-            </a>
-            <a href="https://github.com/AmandaPtela"><img alt="" height="10px" width="10px" id="rede-2"
-              src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white"
-              target="_blank"/>
-            </a>
-            <a href="https://www.linkedin.com/in/amandaptela/"><img alt="" id="rede-3"
+            <a href="https://www.linkedin.com/in/amandaptela/"><img alt="" height="10px" id="rede-2"
               src="https://img.shields.io/badge/-LinkedIn-%230077B5?style=for-the-badge&logo=linkedin&logoColor=white"
-              target="_blank"/>
+              target="_blank" rel="noopener noreferrer" />
             </a>
-            <a href="mailto:mandaptela@gmail.com"><img alt="" height="10px" width="10px" id="rede-4"
-              src="https://img.shields.io/badge/-Gmail-%23333?style=for-the-badge&logo=gmail&logoColor=red"
-              target="_blank"/>
+            <a href="https://github.com/AmandaPtela"><img alt="" height="10px" id="rede-3"
+              src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white"
+              target="_blank" rel="noopener noreferrer" />
+            </a>
+            <a href="mailto:mandaptela@gmail.com" target="_blank" rel="noopener noreferrer" id="rede-4">Send me an email
             </a>
           </div>
         <Link to="/"> <img id="voltar-btn" src={img} /></Link>
